697cc2578c24b31fc15ee018ec174650c7355804
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-06-18 08:51:19 +0000

    Update duplicate-num.py
    created duplicate check code(python)

--- duplicate-num.py
+++ duplicate-num.py
@@ -1,1 +1,14 @@
+# dulicate Function
+def duplicate_num(x):
+        _size = len(x)
+        repeated = []
+        for i in range(_size):
+                k = i + 1
+                for j in range(k, _size):
+                        if x[i] == x[j] and x[i] not in repeated:
+                                repeated.append(x[i])
+        return repeated
 
+# main Code
+list1 = [4, 3, 2, 7, 8, 2, 3, 1]
+print (duplicate_num(list1))
